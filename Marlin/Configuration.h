3f4f288dc848245ac83a22665a7f6ac2b9b3fe87
pyrerat <66904518+pyrerat@users.noreply.github.com>
2020-06-15 15:00:50 -0400

    PID Update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,9 +490,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 27.74
+  #define DEFAULT_Ki 2.35
+  #define DEFAULT_Kd 81.73
 
 #endif // PIDTEMP
 
